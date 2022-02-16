fd2aac1443613a90092272fc843cb92645e7b4dc
Pierce Wang <piercewang@dyn-160-39-144-155.dyn.columbia.edu>
2022-02-16 13:02:11 -0500

    Changed theme

--- .custom.el
+++ .custom.el
@@ -5,19 +5,21 @@
  ;; If there is more than one, they won't work right.
  '(org-modules '(ol-bibtex org-habit))
  '(safe-local-variable-values
-   '((org-roam-dailies-capture-templates
+   '((org-roam-db-location . "/Users/piercewang/Dropbox/org/notes/hacking/notes/course_org-roam.db")
+     (org-roam-directory . "/Users/piercewang/Dropbox/org/notes/hacking/course_notes")
+     (org-roam-dailies-capture-templates
       ("d" "Daily Doodle" plain #'org-roam-capture--get-point "* %?" :file-name "daily/%<%Y-%m-%d>" :head "#+SETUPFILE  ~/.doom.d/org-mode/templates/personal/name_email.org
 #+SETUPFILE: ~/.doom.d/org-mode/templates/general/note.org
 #+SETUPFILE: ~/.doom.d/org-mode/templates/general/web_drawing.org
 #+OPTIONS: toc:nil
-#+roam_tags: 
+#+roam_tags:
 #+title: %u" :unnarrowed t))
      (org-roam-capture-templates
       ("D" "Drawing" plain #'org-roam-capture--get-point "%?" :file-name "%<%Y%m%d%H%M%S>-${slug}" :head "#+SETUPFILE  ~/.emacs.d/org-mode/templates/personal/name_email.org
 #+SETUPFILE: ~/.doom.d/org-mode/templates/general/note.org
 #+SETUPFILE: ~/.doom.d/org-mode/templates/general/web_drawing.org
 #+OPTIONS: toc:nil
-#+roam_tags: 
+#+roam_tags:
 #+title: ${title}
 %U
 " :unnarrowed t))
@@ -30,14 +32,14 @@
 #+SETUPFILE: ~/.emacs.d/org-mode/templates/general/note.org
 #+SETUPFILE: ~/.emacs.d/org-mode/templates/general/web_drawing.org
 #+OPTIONS: toc:nil
-#+roam_tags: 
+#+roam_tags:
 #+title: %u" :unnarrowed t))
      (org-roam-capture-templates
       ("D" "Drawing" plain #'org-roam-capture--get-point "%?" :file-name "%<%Y%m%d%H%M%S>-${slug}" :head "#+SETUPFILE  ~/.emacs.d/org-mode/templates/personal/name_email.org
 #+SETUPFILE: ~/.emacs.d/org-mode/templates/general/note.org
 #+SETUPFILE: ~/.emacs.d/org-mode/templates/general/web_drawing.org
 #+OPTIONS: toc:nil
-#+roam_tags: 
+#+roam_tags:
 #+title: ${title}
 %U
 " :unnarrowed t))
