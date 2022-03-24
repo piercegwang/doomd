c55190b3f52945c70932a1f568c3dd6a32587f7c
Pierce Wang <piercewang@dyn-160-39-144-235.dyn.columbia.edu>
2022-03-24 13:41:32 -0400

    Removed indentation in init

--- .custom.el
+++ .custom.el
@@ -5,7 +5,36 @@
  ;; If there is more than one, they won't work right.
  '(org-modules '(ol-bibtex org-habit))
  '(safe-local-variable-values
-   '((org-roam-db-location . "/Users/piercewang/Dropbox/org/notes/hacking/notes/course_org-roam.db")
+   '((org-roam-dailies-capture-templates
+      ("c" "Class" plain #'org-roam-capture--get-point "%?" :file-name "daily/class-%<%Y%m%d%H%M%S>" :head "#+setupfile: ~/.doom.d/org-mode/templates/general/note.org
+#+setupfile: ~/.doom.d/org-mode/templates/school/name_email.org
+#+setupfile: ~/.doom.d/org-mode/templates/school/writing_setup.org
+#+setupfile: ~/.doom.d/org-mode/templates/general/web.org
+#+roam_tags: discussion
+#+options: toc:nil
+#+title: " :unnarrowed t)
+      ("o" "Office Hours" plain #'org-roam-capture--get-point "%?" :file-name "daily/oh-%<%Y%m%d%H%M%S>" :head "#+setupfile: ~/.doom.d/org-mode/templates/general/note.org
+#+setupfile: ~/.doom.d/org-mode/templates/school/name_email.org
+#+setupfile: ~/.doom.d/org-mode/templates/school/writing_setup.org
+#+setupfile: ~/.doom.d/org-mode/templates/general/web.org
+#+roam_tags: office_hours
+#+options: toc:nil
+#+title: " :unnarrowed t))
+     (org-roam-capture-templates
+      ("r" "Readings" plain #'org-roam-capture--get-point "%?" :file-name "%<%Y%m%d%H%M%S>-${slug}" :head "#+setupfile: ~/.doom.d/org-mode/templates/general/note.org
+#+setupfile: ~/.doom.d/org-mode/templates/school/name_email.org
+#+setupfile: ~/.doom.d/org-mode/templates/school/writing_setup.org
+#+setupfile: ~/.doom.d/org-mode/templates/general/web.org
+#+roam_tags: readings
+#+options: toc:nil
+#+title: ${title}
+" :unnarrowed t)
+      ("t" "Topic" plain #'org-roam-capture--get-point "%?" :file-name "%<%Y%m%d%H%M%S>-${slug}" :head "#+roam_tags: topic
+#+title: ${title}
+" :unnarrowed t))
+     (org-roam-db-location . "/Users/piercewang/Documents/OCRA1/notes/org-roam.db")
+     (org-roam-directory . "/Users/piercewang/Documents/OCRA1/notes/")
+     (org-roam-db-location . "/Users/piercewang/Dropbox/org/notes/hacking/notes/course_org-roam.db")
      (org-roam-directory . "/Users/piercewang/Dropbox/org/notes/hacking/course_notes")
      (org-roam-dailies-capture-templates
       ("d" "Daily Doodle" plain #'org-roam-capture--get-point "* %?" :file-name "daily/%<%Y-%m-%d>" :head "#+SETUPFILE  ~/.doom.d/org-mode/templates/personal/name_email.org
